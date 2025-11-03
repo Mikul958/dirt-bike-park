9110e1c52bec49e0ace8f4f6b809cd1dcd041510
Tyler Stargel <thetylerstargel@gmail.com>
2025-11-03 08:53:35 -0500

    database stubs

---
+++ DirtBikePark/Data/DatabaseContext.cs
@@ -0,0 +1,15 @@
+﻿using DirtBikePark.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DirtBikePark.Data
+{
+    public class DatabaseContext : DbContext
+    {
+        public DbSet<Booking> Bookings { get; set; }
+        public DbSet<Park> Parks { get; set; }
+        public DbSet<Cart> Carts { get; set; }
+
+        public DatbaseContext(DbContextOptions<DatabaseContext> options) : base(options) { }
+
+    }
+}
