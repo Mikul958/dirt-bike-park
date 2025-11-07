1d821f8e1440790dc33be8accd9006ce5786ca0a
Tyler Stargel <thetylerstargel@gmail.com>
2025-11-07 15:13:06 -0500

    modifed launchsettings to open swaggerUI for testing, modified ParkService to include relevant calls to the database.

--- DirtBikePark/Services/ParkService.cs
+++ DirtBikePark/Services/ParkService.cs
@@ -1,3 +1,4 @@
+using DirtBikePark.Data;
 using DirtBikePark.Interfaces;
 using DirtBikePark.Models;
 using System;
@@ -9,24 +10,34 @@ namespace DirtBikePark.Services
 	public class ParkService : IParkService
     {
 		// written with mock data
-		private readonly List<Park> _parks = new List<Park>
+		//private readonly List<Park> _parks = new List<Park>
+		//{
+		//	new Park { Id = 1, Name = "Park One", Description = "There are a lot of trees.", GuestLimit = 100, PricePerAdult = 25.00m, PricePerChild = 15.00m },
+		//	new Park { Id = 2, Name = "Park Two", Description = "There is a river through the middle.", GuestLimit = 100, PricePerAdult = 25.00m, PricePerChild = 15.00m  },
+		//	new Park { Id = 3, Name = "Park Three", Description = "It's pretty green.", GuestLimit = 100, PricePerAdult = 25.00m, PricePerChild = 15.00m  }
+		//};
+
+		private readonly DatabaseContext _context;
+
+        public ParkService(DatabaseContext context)
+        {
+			_context = context;
+        }
+        public Task<Park?> GetPark(int parkId)
 		{
-			new Park { Id = 1, Name = "Park One", Description = "There are a lot of trees.", GuestLimit = 100, PricePerAdult = 25.00m, PricePerChild = 15.00m },
-			new Park { Id = 2, Name = "Park Two", Description = "There is a river through the middle.", GuestLimit = 100, PricePerAdult = 25.00m, PricePerChild = 15.00m  },
-			new Park { Id = 3, Name = "Park Three", Description = "It's pretty green.", GuestLimit = 100, PricePerAdult = 25.00m, PricePerChild = 15.00m  }
-		};
-		public Task<Park?> GetPark(int parkId)
-		{
-			var park = _parks.Find(p => p.Id == parkId);
-			return Task.FromResult(park);
+            //var park = _parks.Find(p => p.Id == parkId);
+            var park = _context.Parks.Find(parkId);
+            return Task.FromResult(park);
 		}
 		
 		public Task<IEnumerable<Park>> GetParks()
 		{
-			return Task.FromResult<IEnumerable<Park>>(_parks);
-		}
-		
-		public Task<bool> AddPark(Park park)
+            //return Task.FromResult<IEnumerable<Park>>(_parks);
+            return Task.FromResult<IEnumerable<Park>>(_context.Parks);
+
+        }
+
+        public Task<bool> AddPark(Park park)
         {
             if (park == null)
                 return Task.FromResult(false);
@@ -35,23 +46,33 @@ namespace DirtBikePark.Services
                 return Task.FromResult(false);
 
             int assignedId;
-			if (park.Id > 0 && !_parks.Any(p => p.Id == park.Id))
-				assignedId = park.Id;
-			else
-				assignedId = _parks.Any() ? _parks.Max(p => p.Id) + 1 : 1;
+            //if (park.Id > 0 && !_parks.Any(p => p.Id == park.Id))
+            //	assignedId = park.Id;
+            //else
+            //	assignedId = _parks.Any() ? _parks.Max(p => p.Id) + 1 : 1;
+
+            if (park.Id > 0 && !_context.Parks.Any(p => p.Id == park.Id))
+                assignedId = park.Id;
+            else
+                assignedId = _context.Parks.Any() ? _context.Parks.Max(p => p.Id) + 1 : 1;
 
             park.Id = assignedId;
-			_parks.Add(park);
-			
-			return Task.FromResult(true);  // return success
+			//_parks.Add(park);
+			_context.Parks.Add(park);
+            _context.SaveChanges();
+
+            return Task.FromResult(true);  // return success
 		}
 		
 		public Task<bool> RemovePark(int parkId)
 		{
-			var park = _parks.FirstOrDefault(p => p.Id == parkId);
+			//var park = _parks.FirstOrDefault(p => p.Id == parkId);
+			var park = _context.Parks.FirstOrDefault(p => p.Id == parkId);
 			if (park == null)
 				return Task.FromResult(false);
-			_parks.Remove(park);
+			//_parks.Remove(park);
+			_context.Parks.Remove(park);
+			_context.SaveChanges();
 			return Task.FromResult(true);
 		}
 	}
