4bd0bb72cf2393fd2348f6b0bed8c383cfd6a2c3
Tyler Stargel <thetylerstargel@gmail.com>
2025-11-16 22:00:19 -0500

    added initial cart repository

---
+++ DirtBikePark/Data/CartRepository.cs
@@ -0,0 +1,34 @@
+﻿using DirtBikePark.Interfaces;
+using DirtBikePark.Models;
+
+namespace DirtBikePark.Data
+{
+    public class CartRepository : ICartRepository
+    {
+        private readonly DatabaseContext _context;
+
+        public CartRepository(DatabaseContext context)
+        {
+            _context = context;
+        }
+        public void AddCart(Cart cart)
+        {
+            _context.Carts.Add(cart);
+        }
+
+        public Cart? GetCart(Guid? cartId)
+        {
+            return _context.Carts.FirstOrDefault(cart => cart.Id == cartId);
+        }
+
+        //public void RemoveCart(Guid cartId)
+        //{
+        //    _context.Carts.Remove(cartId);
+        //}
+
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
+    }
+}
---
+++ DirtBikePark/Interfaces/ICartRepository.cs
@@ -0,0 +1,12 @@
+﻿using DirtBikePark.Models;
+
+namespace DirtBikePark.Interfaces
+{
+    public interface ICartRepository
+    {
+        Cart? GetCart(Guid? cartId);
+        void AddCart(Cart cart);
+        //void RemoveCart(Guid cartId);
+        void Save();
+    }
+}
