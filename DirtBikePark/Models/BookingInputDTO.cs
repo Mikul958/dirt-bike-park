a7bef9bf993ba13c67be6f0359bcddf96ee6edf0
Mikul958 <michaelpikula958@gmail.com>
2025-11-21 16:09:09 -0500

    Add DTOs for booking

---
+++ DirtBikePark/Models/BookingInputDTO.cs
@@ -0,0 +1,28 @@
+﻿namespace DirtBikePark.Models
+{
+    public class BookingInputDTO
+    {
+        public int Id { get; set; }
+        public int NumDays { get; set; }
+        public int NumAdults { get; set; }
+        public int NumChildren { get; set; }
+
+        // Creates a new Booking from the provided DTO information.
+        // NOTE: Only expected to be used on POSTs, PUTs reference BookingId only
+        public Booking FromInputDTO(BookingInputDTO bookingDTO)
+        {
+            Booking bookingModel = new Booking()
+            {
+                Id = bookingDTO.Id,
+                CartId = null,
+                ParkId = 0,
+                NumDays = bookingDTO.NumDays,
+                NumAdults = bookingDTO.NumAdults,
+                NumChildren = bookingDTO.NumChildren,
+                TotalPrice = 0,
+                IsPaidFor = false
+            };
+            return bookingModel;
+        }
+    }
+}
---
+++ DirtBikePark/Models/BookingResponseDTO.cs
@@ -0,0 +1,30 @@
+﻿namespace DirtBikePark.Models
+{
+    public class BookingResponseDTO
+    {
+        public int Id { get; set; }
+        public Park? Park { get; set; }
+        public int NumDays { get; set; }
+        public int NumAdults { get; set; }
+        public int NumChildren { get; set; }
+        public decimal TotalPrice { get; set; }
+
+        // Creates a new BookingResponseDTO with information provided in a Booking
+        public BookingResponseDTO(Booking bookingModel)
+        {
+            Id = bookingModel.Id;
+            Park = bookingModel.Park;
+            NumDays = bookingModel.NumDays;
+            NumAdults = bookingModel.NumAdults;
+            NumChildren = bookingModel.NumChildren;
+            TotalPrice = bookingModel.TotalPrice;
+        }
+
+        // Takes an existing BookingDTO and supplies it with information in a Booking
+        public BookingResponseDTO ToResponseDTO(Booking bookingModel)
+        {
+            BookingResponseDTO bookingDTO = new BookingResponseDTO(bookingModel);
+            return bookingDTO;
+        }
+    }
+}
