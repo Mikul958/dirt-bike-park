1a52ed7409c84b6935a529ae279aa29736274208
Mikul958 <michaelpikula958@gmail.com>
2025-11-21 16:29:21 -0500

    Fixes

--- DirtBikePark/Models/Payment.cs
+++ DirtBikePark/Models/Payment.cs
@@ -5,9 +5,6 @@ namespace DirtBikePark.Models
 {
     public class Payment
     {
-        [Key]
-        public int Id { get; set; }
-        [ForeignKey("Cart")]
         public Guid CartId { get; set; }
         public string CardNumber { get; set; } = string.Empty;
         public string Ccv { get; set; } = string.Empty;
