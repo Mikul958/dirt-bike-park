499b505035eec9d747f14270fc6c8d6c2ac9aeac
Mikul958 <michaelpikula958@gmail.com>
2025-11-21 16:28:51 -0500

    Fixes

---
+++ DirtBikePark/Models/ParkInputDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace DirtBikePark.Models
+{
+    public class ParkInputDTO
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Location { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string ImageUrl { get; set; } = string.Empty;
+        public decimal PricePerAdult { get; set; }
+        public decimal PricePerChild { get; set; }
+        public int GuestLimit { get; set; }
+    }
+}
